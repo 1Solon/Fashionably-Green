92a235543a95754424f798449b3e0108b93b1396
Solon <Solonerus@gmail.com>
2023-07-23 15:21:03 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt" ,
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "My App",
+    short_name: "App",
+    description: "My App description",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/maskable-icon.svg",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "./",
+    plugins: [react(), VitePWA(manifestForPlugin)],
+});
